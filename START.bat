bdb21f221e8c045ba5320739abb29ed93a07279b
PavelTonNik <pavel.tonenkov@gmail.com>
2025-12-28 20:58:54 +0400

    удалил не нужное

--- START.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 > nul
-cd /d "%~dp0"
-echo ================================
-echo   STARTING FASTAPI SERVER
-echo ================================
-echo.
-start cmd /k "python run_server.py"
--- run_migration.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ================================
-echo   DATABASE MIGRATION
-echo ================================
-echo.
-python migrate_db.py
-echo.
-pause
